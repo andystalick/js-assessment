e1709ed371484da03564efab3fb9b055d0e67070
Andrew Stalick <astalick@rmn.com>
2018-05-31 13:06:18 -0500

    some updates from the work sessions

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,18 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
-
-  },
+  alterContext: function(fn, obj) {},
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    var res = [];
+    var keys = Object.keys(obj);
+    for (i = 0; i < keys.length; i++) {
+      res.push(`${keys[i]}: ${obj[keys[i]]}`);
+    }
+    return res;
   }
 };
